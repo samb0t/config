dc18a4b43ea1f7c400f549737317bfd791f011c6
samb0t <sambottoni@gmail.com>
2025-05-13 17:09:26 -0500

    copy to media drive to force resync

--- ps/MediaBackup.ps1
+++ ps/MediaBackup.ps1
@@ -1,10 +1,11 @@
 # Need Set-ExecutionPolicy Unrestricted
 # Used in a daily Task Scheduler
 #   Run a program: powershell.exe
-#   Arguments (because windows is insane): -Command " &'C:\Users\burbo\Documents\My Projects\GitHub\config\ps\MediaBackup.ps1'"
-#   Start in (no quotes): C:\Users\burbo\Documents\My Projects\GitHub\config\ps 
+#   Arguments (because windows is insane): -Command " &'C:\Users\sambo\Documents\My Projects\GitHub\config\ps\MediaBackup.ps1'"
+#   Start in (no quotes): C:\Users\sambo\Documents\My Projects\GitHub\config\ps 
 # NOTE: Be careful with /MIR as it will blow away files that are extra in the destination - CHECK YOUR SRC/DEST
 
 $todaysDate = get-date -format yyyy-MM-dd
 
 robocopy y: m:/mac-backup /r:3 /e /IT /NDL /LOG+:"$($todaysDate)-Media.log"
+Copy-Item -Path "$($todaysDate)-Media.log" -Destination "m:\"
--- ps/PCBackup.ps1
+++ ps/PCBackup.ps1
@@ -1,8 +1,8 @@
 # Need Set-ExecutionPolicy Unrestricted
 # Used in a daily Task Scheduler
 #   Run a program: powershell.exe
-#   Arguments (because windows is insane): -Command " &'C:\Users\burbo\Documents\My Projects\GitHub\config\ps\PCBackup.ps1'"
-#   Start in (no quotes): C:\Users\burbo\Documents\My Projects\GitHub\config\ps 
+#   Arguments (because windows is insane): -Command " &'C:\Users\sambo\Documents\My Projects\GitHub\config\ps\PCBackup.ps1'"
+#   Start in (no quotes): C:\Users\sambo\Documents\My Projects\GitHub\config\ps 
 
 param ($rootDest)
 if ($rootDest -eq $null) {
@@ -10,7 +10,7 @@ if ($rootDest -eq $null) {
     return
 }
 
-$rootSource = "c:\users\burbo"
+$rootSource = "c:\users\sambo"
 
 $backupDirs = 
     "Documents\My Audio",
